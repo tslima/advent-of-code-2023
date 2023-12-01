d6edc0e31ff7f587f19cfad4f119a6c6270d97a8
tiago <tiago@pop-os.localdomain>
2023-12-01 17:12:21 -0300

    primeiro desafio

---
+++ day-01/src/main.rs
@@ -0,0 +1,76 @@
+use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn test_1() {
+    let text = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+
+    if calibration_value(text) == 142 {
+        println!("SUCESSO")
+    } else {
+        println!("FALHA")
+    }
+}
+
+fn etapa_1() {
+    let mut file = File::open("input.txt").expect("ERRO");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("ERRO");
+    calibration_value(&contents);
+}
+
+fn teste_2() {
+    let text = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+    if calibration_value(text) == 281 {
+        println!("SUCESSO");
+    } else {
+        println!("FALHA");
+    }
+}
+
+fn main() {
+    let mut file = File::open("input2.txt").expect("ERRO");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("ERRO");
+    calibration_value(&contents);
+}
+
+fn calibration_value(text: &str) -> u32 {
+    const RADIX: u32 = 10;
+    let re = Regex::new(r"[A-z]").unwrap();
+    let re_text = text
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e");
+    let filter_text = re.replace_all(&re_text, "");
+
+    let lines = filter_text.lines();
+
+    let mut total = 0;
+    for line in lines {
+        if line.len() > 0 {
+            let first: u32 = line.chars().next().unwrap().to_digit(RADIX).unwrap();
+            let last: u32 = line.chars().last().unwrap().to_digit(RADIX).unwrap();
+            println!("{}", first * 10 + last);
+            total += first * 10 + last;
+        }
+    }
+    println!("{}", total);
+    total
+}
