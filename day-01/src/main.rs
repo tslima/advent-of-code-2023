dc6771a91dd6d9e82392d24eb7b02e02b67964bc
tiago <tiago@pop-os.localdomain>
2023-12-01 18:55:06 -0300

    chore: testes transformados em testes unitários

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,26 +2,17 @@ use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[test]
 fn test_1() {
     let text = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
 
-    if calibration_value(text) == 142 {
-        println!("SUCESSO")
-    } else {
-        println!("FALHA")
-    }
+    assert_eq!(calibration_value(text), 142);
 }
 
-fn etapa_1() {
-    let mut file = File::open("input.txt").expect("ERRO");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("ERRO");
-    calibration_value(&contents);
-}
-
+#[test]
 fn teste_2() {
     let text = "two1nine
 eightwothree
@@ -31,18 +22,23 @@ xtwone3four
 zoneight234
 7pqrstsixteen";
 
-    if calibration_value(text) == 281 {
-        println!("SUCESSO");
-    } else {
-        println!("FALHA");
-    }
+    assert_eq!(calibration_value(text), 281);
 }
 
 fn main() {
+    let mut file = File::open("input.txt").expect("ERRO");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("ERRO");
+    let etapa_1 = calibration_value(&contents);
+
+    println!("Etapa 1: {}", etapa_1);
+
     let mut file = File::open("input2.txt").expect("ERRO");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("ERRO");
-    calibration_value(&contents);
+    let etapa_2 = calibration_value(&contents);
+
+    println!("Etapa 2: {}", etapa_2);
 }
 
 fn calibration_value(text: &str) -> u32 {
@@ -67,10 +63,8 @@ fn calibration_value(text: &str) -> u32 {
         if line.len() > 0 {
             let first: u32 = line.chars().next().unwrap().to_digit(RADIX).unwrap();
             let last: u32 = line.chars().last().unwrap().to_digit(RADIX).unwrap();
-            println!("{}", first * 10 + last);
             total += first * 10 + last;
         }
     }
-    println!("{}", total);
     total
 }
